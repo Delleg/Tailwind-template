71a35c3b12887d75ddd69b78394c1bd00b94f182
Artur <arturorysz@gmail.com>
2024-01-07 00:14:51 +0100

    new  header wit flex

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 module.exports = {
 	content: ['index.html', './src/**/*.{html,js}'],
 	theme: {
-		extend: {},
+		extend: {
+			zIndex: {
+				100: '100',
+			},
+			colors: {
+				transparent: 'transparent',
+				current: 'currentColor',
+				'bright-blue': '#1864AB',
+				'dark-blue': '#0F1322',
+				'paper-white': '#E8E8E8',
+			},
+		},
+
+		fontFamily: {
+			'source-sans-3': ['"Source Sans 3"', 'serif'],
+			arimo: ['Arimo', 'serif'],
+		},
 	},
 	plugins: [],
 };
