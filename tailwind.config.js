f1f53d0f3e19f40ea1ab69c083111a02e730b983
Artur <arturorysz@gmail.com>
2024-02-19 14:16:27 +0100

    Navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,22 +2,40 @@
 module.exports = {
 	content: ['index.html', './src/**/*.{html,js}'],
 	theme: {
+		container: {
+			center: true,
+			padding: '1rem',
+			screens: {
+				sm: '600px',
+				md: '728px',
+				lg: '1440px',
+				xl: '1496px',
+				// '2xl': '1496px',
+			},
+		},
 		extend: {
 			zIndex: {
 				100: '100',
 			},
 			colors: {
-				transparent: 'transparent',
-				current: 'currentColor',
-				'bright-blue': '#1864AB',
-				'dark-blue': '#0F1322',
-				'paper-white': '#E8E8E8',
+				Danger: 'hsl(0, 83%, 62%)',
+				Primary: 'hsl(199, 87%, 61%)',
+				Success: 'hsl(165, 80%, 43%)',
+				Warning: 'hsl(38, 89%, 68%)',
+				Info: 'hsl(184, 100%, 28%)',
+				'Brand-1': 'hsl(27, 100%, 82%)',
+				'Brand-2': 'hsl(26, 88%, 90%)',
+				'Brand-3': 'hsl(18, 100%, 95%)',
+				'Brand-4': 'hsl(23, 100%, 97%)',
+				'Grey-900': 'hsl(0, 0%, 6%)',
+				'Grey-700': 'hsl(0, 0%, 28%)',
+				'Grey-400': 'hsl(0, 0%, 60%)',
+				'Grey-100': 'hsl(0, 0%, 95%)',
 			},
 		},
 
 		fontFamily: {
-			'source-sans-3': ['"Source Sans 3"', 'serif'],
-			arimo: ['Arimo', 'serif'],
+			Source_Sans_3: ['Source Sans 3', 'sans-serif'],
 		},
 	},
 	plugins: [],
